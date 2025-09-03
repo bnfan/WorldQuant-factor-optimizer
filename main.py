cfd14798762be2031f5ee4e77a8e63e0e6ce00f5
bnfan <bnfan@qq.com>
2025-09-03 12:17:33 +0800

    new main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+from gpt_optimizer import WorldQuantFactorOptimizer
+
+express = '-ts_mean((close/open - 1), 10))'
+
+# 创建优化器实例
+optimizer = WorldQuantFactorOptimizer(express)
+
+# 运行优化流程
+optimizer.run_optimization()
